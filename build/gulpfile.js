f80e821a113afb366fcd96260521bdbfc7d0c73d
the-sides <jsides@pyxl.com>
2019-07-08 17:58:43 -0400

    the build tools of destiny

---
+++ build/gulpfile.js
@@ -0,0 +1,32 @@
+const {src, dest, series, watch} = require('gulp')
+const webpack = require('webpack-stream')
+
+function styles(){
+    return src('../src/styles/**/*.css')
+            .pipe(dest('dist/styles/'));
+}
+function cometGame(){
+    return src('../src/scripts/comet.js')
+            .pipe(webpack(require('./webpack.config')))
+            .pipe(dest('dist/scripts/'));
+}
+function brawlGame(){
+    return src('../src/scripts/brawlbots/brawlbots.js')
+            .pipe(webpack(require('./webpack.config')))
+            .pipe(dest('dist/scripts/'));
+} 
+function watcher(cb){
+    watch(
+        [
+          `../src/**/*.js`,
+        ],
+        series(
+            cometGame
+        )
+    );
+    cb();
+}
+
+const dev = series(cometGame, styles, watcher)
+
+module.exports = {dev, watcher}

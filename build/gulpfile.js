7ab58bc48ab0ed59d6d806ff5d4b90f72aab42bf
the-sides <jsides@pyxl.com>
2019-07-21 19:59:30 -0400

    browsersync automated reloaded

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,5 +1,27 @@
-const {src, dest, series, watch} = require('gulp')
+const {src, dest, series, parallel} = require('gulp')
+const watch = require('glob-watcher')
 const webpack = require('webpack-stream')
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
+
+async function bsTask(){
+    console.log('this is bs');
+    return browserSync.init({
+        proxy: "localhost:3000", 
+        port: 3001
+    })
+}
+async function nodemonTask(cb) {
+        return nodemon( {
+            script: '../bin/www', 
+            ext: 'js html', 
+            env: { 'NODE_ENV': 'development' }, 
+            port: 3000, 
+            done: cb()
+        })//.once('start', cb);
+
+};
+
 
 function styles(){
     return src('../src/styles/**/*.css')
@@ -18,16 +40,23 @@ function brawlGame(){
 
 const dev = series(cometGame, styles)
 
-function watcher(cb){
-    dev();
-    watch(
-        [
-          `../src/**/*.js`,
-        ],
-        dev()
-    );
-    cb();
+async function watcher(){
+    // dev();
+    watch( [ `../src/**/**`, `../views/**/*.pug`], ()=>{
+        series(
+            // dev(),
+            cometGame(), 
+            styles(),
+            browserSync.reload() )
+
+    });
 }
 
+// const run = series(nodemonTask, bsTask, watcher);
 
-module.exports = {dev, watcher}
+const run = series(watcher, nodemonTask, bsTask);
+// async function run(){ return series(nodemonTask, bsTask, watcher) }
+// const run = parallel(bsTask, nodemonTask);
+// const run = () => {nodemonTask(bsTask)}//series(bsTask, nodemonTask);
+
+module.exports = {dev, watcher, run}
